69c568640f9ac0e6cead9f3489e7e62a7fd2f567
G P <gp@Gs-MacBook-Air.local>
2026-05-03 10:17:27 +0300

    Fix check-push.js to run from backend dir

--- check-push.js
+++ check-push.js
@@ -1,6 +1,7 @@
-require('dotenv').config({ path: './backend/.env' });
-const db = require('./backend/src/db/database');
+process.chdir('./backend');
+require('dotenv').config();
+const db = require('./src/db/database');
 const subs = db.prepare('SELECT user_id, created_at FROM push_subscriptions').all();
 console.log('VAPID configured:', !!process.env.VAPID_PUBLIC_KEY);
 console.log('Push subscriptions:', subs.length);
-console.log(subs);
+console.log(JSON.stringify(subs, null, 2));
